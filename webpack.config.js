416eb903379562691737f7c9848ca45a7b30007d
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-17 08:45:22 -0300

    Cargando la configuración de webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = requiere('path');
+const HtmlWebpackPlugin = requiere('html-webpack-plugin');
+
+//Objetos de configuración
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve:{
+        extensions:['.js','jsx'],
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test:/\.html$/,
+                use:[
+                    {
+                        loader:'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+    ]
+}
