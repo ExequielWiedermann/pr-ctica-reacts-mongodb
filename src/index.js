64315089380dcd735c27fedaf61b125bc7e22a70
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-18 19:50:32 -0300

    Creando el constructor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 //uso de clases para componentes
 class App extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            count: 0
+        };
+    }
+
     render(){
         return(
             <div>
