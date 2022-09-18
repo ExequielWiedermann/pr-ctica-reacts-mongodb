48f048c933e31d39dd4ca4fce45c532605043c69
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-18 18:59:51 -0300

    Cambiando el home de mi proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Header from '../components/Header';
 
 const Home = () => {
     return(
-        <div>
-            <h1>React principal"</h1>
-        </div>
+        <Header />
     );
 }
+
+export default Home;
