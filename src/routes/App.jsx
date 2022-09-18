cc87f00e34fe1fed91dc52446d98d55d8ae75cfd
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-18 18:00:59 -0300

    Agregando el import en routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,8 @@ import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import '../styles/global.scss';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 
 const App = () => {
     return (
