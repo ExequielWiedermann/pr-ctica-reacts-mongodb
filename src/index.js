20ee2873c94068e1bf30f93ba65a7016c36e87c3
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-18 19:36:14 -0300

    Creando el stateful

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Button = () =>{
     //Para trabajar con stateful y stateless
-    const [name, setName] = useName('Hola');
+    const [name, setName] = useName('Hola Mundo');
     return(
         <div>
-            <h1>Hola mundos</h1>
+            <h1>{name}</h1>
         </div>
     );
 }
