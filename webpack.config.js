bc9594c0f33aef63fc450f3ad2a69b7863bf3ee6
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-18 01:31:33 -0300

    Cambiando webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,51 @@
-const path = requiere('path');
-const HtmlWebpackPlugin = requiere('html-webpack-plugin');
-const MiniCssExtractPlugin = requiere('mini-css-extract-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-//Objetos de configuración
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        stats: {
-          modules: false,
-          cached: false,
-          colors: true,
-          chunk: false
-        },
-    },
-    mode: 'development',
-    resolve:{
-        extensions:['.js','jsx'],
-    },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                use:{
-                    loader:'babel-loader'
-                },
-                loaders: [
-                    require.resolve('react-hot-loader'),
-                    require.resolve('babel-loader')
-                 ]
-            },
-            {
-                test:/\.html$/,
-                use:[
-                    {
-                        loader:'html-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ],
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-    ],
-    devServer:{
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3000,
-    }
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	],
 }
